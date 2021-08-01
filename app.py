5ed34f7b17156bfc5e7ebc4657ff7bec1bdedbb8
lonnie <lonnie@souderfamily.net>
2021-07-31 21:40:07 -0400

    Works with confidental client.

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, Response, request, redirect
+import service
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def current_bulletin():
-    return redirect("https://www.vbchammonton.org/uploads/3/7/1/8/3718617/july_25th_bulletin.pdf", code=302)
+    return redirect(service.get_bulletin_url(), code=302)
+
+@app.route('/test-bull')
+def test():
+    return redirect(service.get_bulletin_url(), code=302)
+
+@app.route('/see-bull')
+def seeUrl():
+    return service.show_login()
+    #return "testing 1 2 3."
 
 if __name__=="__main__":
       app.run()
--- service.py
+++ service.py
@@ -1,9 +1,13 @@
+import logging
 import requests
+import confidential_client
 import msal
 import atexit
 import os.path
 import json
 from datetime import datetime
+
+config = json.load(open("parameters.json"))
 
 TENANT_ID = '356e67d4-d13d-44fe-815b-3fb842925be4'
 CLIENT_ID = '208496d8-671c-4f89-8df6-ceded22c8a2e'
@@ -19,36 +23,11 @@ SCOPES = [
     'User.ReadBasic.All'
 ]
 
+user_id = "users/81ac1f9a-b470-4af0-8018-2fd61644d6e9"
 drive_id = "b!5ep1PXhxG0Km5Jqkf-u_DFufdhWHEjlHu4PtVKswFOR5Qfty_KoNSbRJUCcXIzx5"
 bulletin_folder_id = "01STA2XAXZOOCEYHHI6FH2WGUNOAN6LYJU"
 
-def webhook():
-  result = getAccessToken()
-
-  if 'access_token' in result:
-    access_token =  result['access_token']
-    
-    list_bulletin_folder = f'{ENDPOINT}/me/drive/root/children'
-    result = requests.get(list_bulletin_folder, headers={'Authorization': 'Bearer ' + access_token})
-    result.raise_for_status()
-    json_data = result.json()
-    for values in json_data["value"]:
-      print(f'{values["name"]} {values["id"]}')
-
-    subscribe = { "changeType": "updated",
-                  "notificationUrl": "https://86168d5b2f19.ngrok.io/api/web-receiver",
-                  "resource": "/me/drive/root",
-                  "expirationDateTime":"2021-04-26T18:23:45.0Z",
-                  "clientState": "client-specific string" }
-                 
-    print(subscribe)
-    # folder_id=next(item for item in json_data["value"] if item["name"] == "web-hook-test")["id"]
-    # print(folder_id)
-    subscribe_url=f'{ENDPOINT}/subscriptions'
-    result=requests.post(subscribe_url, json=subscribe, headers={'Authorization': 'Bearer ' + access_token})
-    print(result.json())
-
-def getAccessToken():
+def get_access_token():
   cache = msal.SerializableTokenCache()
 
   if os.path.exists('token_cache.bin'):
@@ -68,26 +47,31 @@ def getAccessToken():
     if 'user_code' not in flow:
         raise Exception('Failed to create device flow')
 
-    print(flow['message'])
+    logging.info(flow['message'])
 
     result = app.acquire_token_by_device_flow(flow)
     
   return result
 
-def getLatestBulletin():
-  
-  result = getAccessToken()
-
-  if 'access_token' in result:
+def list_bulletin_folder(result):
     access_token =  result['access_token']
-
-    list_bulletin_folder = f'{ENDPOINT}/me/drives/{drive_id}/items/{bulletin_folder_id}/children'
+    list_bulletin_folder = f'{ENDPOINT}/{user_id}/drives/{drive_id}/items/{bulletin_folder_id}/children'
     result = requests.get(list_bulletin_folder, headers={'Authorization': 'Bearer ' + access_token})
     result.raise_for_status()
     json_data = result.json()
+    return json_data
+
+def get_bulletin_url():
+  result = confidential_client.get_access_token(config)
+  return get_url_to_latest_bulletin(result)
+
+def get_url_to_latest_bulletin(result):
+  if 'access_token' in result:
+    json_data = list_bulletin_folder(result)
+    access_token = result['access_token']
     most_recent = max(json_data["value"], key = lambda k: datetime.strptime(k["lastModifiedDateTime"], "%Y-%m-%dT%H:%M:%SZ"))
 
-    share_link = f'{ENDPOINT}/me/drive/items/{most_recent["id"]}/createlink'
+    share_link = f'{ENDPOINT}/{user_id}/drive/items/{most_recent["id"]}/createlink'
     anonymous_view={'type':'view', 'scope':'anonymous'}
     result = requests.post(share_link, headers={'Authorization': f'Bearer {access_token}'}, json=anonymous_view )
     result.raise_for_status()
@@ -96,10 +80,11 @@ def getLatestBulletin():
     
 
   else:
-    raise Exception('no access token in result')
+    return "Nothing"
+    #raise Exception('no access token in result')
 
 def list_subs():
-  result = getAccessToken()
+  result = get_access_token()
   if 'access_token' in result:
     access_token =  result['access_token']
     
@@ -108,22 +93,34 @@ def list_subs():
     result.raise_for_status()
     json_data = result.json()
     for sub_entry in json_data["value"]:
-      print(sub_entry["id"])
+      logging.info(sub_entry["id"])
 
-def del_subs():
-  result = getAccessToken()
-  if 'access_token' in result:
-    access_token =  result['access_token']
+def show_login():
+
+  cache = msal.SerializableTokenCache()
+
+  if os.path.exists('token_cache.bin'):
+    cache.deserialize(open('token_cache.bin', 'r').read())
+
+  atexit.register(lambda: open('token_cache.bin', 'w').write(cache.serialize()) if cache.has_state_changed else None)
+
+  app = msal.PublicClientApplication(CLIENT_ID, authority=AUTHORITY, token_cache=cache)
+
+  accounts = app.get_accounts()
+  result = None
+  if len(accounts) > 0:
+    result = app.acquire_token_silent(SCOPES, account=accounts[0])
+
+  if result is None:
+    flow = app.initiate_device_flow(scopes=SCOPES)
+    if 'user_code' not in flow:
+        raise Exception('Failed to create device flow')
+
+    return (flow['message'])
+
+    result = app.acquire_token_by_device_flow(flow)
     
-    subscriptions = f'{ENDPOINT}/subscriptions'
-    result = requests.get(subscriptions, headers={'Authorization': 'Bearer ' + access_token})
-    result.raise_for_status()
-    json_data = result.json()
-    for sub_entry in json_data["value"]:
-      delete_sub = f'{ENDPOINT}/subscriptions/{sub_entry["id"]}'
-      result = requests.delete(f'{delete_sub}', headers={'Authorization': 'Bearer ' + access_token})
-      result.raise_for_status()
-      print(sub_entry["id"])
+  return "nothing"
 
 if __name__=="__main__":
-      getLatestBulletin()
+      logging.info(get_bulletin_url())
