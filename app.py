bd438ff5c1b7d6f90f0a4d56e6b44602790daa45
Lonnie Souder <lonnie@souderfamily.net>
2021-09-06 08:34:46 -0400

    Clean up. put webhook back for testing.

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import service
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def current_bulletin():
     return redirect(service.get_bulletin_url(), code=302)
 
-@app.route('/test-bull')
-def test():
-    return redirect(service.get_bulletin_url(), code=302)
-
-@app.route('/see-bull')
-def seeUrl():
-    return service.show_login()
-    #return "testing 1 2 3."
+@app.route('/api/web-receiver', methods=['POST'])
+def respond():
+    valicationToken = request.args.get("validationToken")
+    if valicationToken is not None:
+        print("sending back validation.")
+        return valicationToken
+    request_value = request.json["value"][0]
+    print("post: ", request_value["resource"], request_value["changeType"])
+    return ""
 
 if __name__=="__main__":
-      app.run()
+      app.run(debug=True)
